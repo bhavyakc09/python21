ac1e49cf8549e8a637a39c9815128e782fa0b801
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 18:03:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ workbook = openpyxl.load_workbook('input_data.xlsx')
 # Select the first sheet
 sheet = workbook.active
 
+# Initialize variables to hold the input data
+name = None
+age = None
+
 # Iterate over the rows and columns to read data
-for row in sheet.iter_rows(values_only=True):
-    for cell in row:
-        print(cell)
+for row in sheet.iter_rows(min_row=2, values_only=True):
+    name, age = row
+    print(f"Name: {name}, Age: {age}")
+
