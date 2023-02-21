860dc8e058f1f3b883b57cdff0bc4cb467bfa924
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 17:57:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
 import pandas as pd
-import openpyxl
-import argparse
 
-def read_input_data(input_file):
-    df = pd.read_excel(input_file)
-    return df
+# read data from Excel file
+data = pd.read_excel('input_data.xlsx')
 
-def process_input_data(df):
-    print("Column names:", df.columns)
-    df['output'] = df['input'] * 2
-    return df
+# process data (replace with your own processing logic)
+processed_data = data * 2
 
-def write_output_data(output_file, df):
-    writer = pd.ExcelWriter(output_file, engine='openpyxl')
-    writer.book = openpyxl.Workbook()
-    df.to_excel(writer, index=False)
-    writer.save()
-
-def process_excel(input_file, output_file):
-    df = read_input_data(input_file)
-    df = process_input_data(df)
-    write_output_data(output_file, df)
-    
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', type=str, help='Path to input Excel file')
-    parser.add_argument('--output_file', type=str, help='Path to output Excel file')
-    args = parser.parse_args()
-    
-    input_file = args.input_file
-    output_file = args.output_file
-    
-    process_excel(input_file, output_file)
+# save output to a text file
+with open('output_data.txt', 'w') as f:
+    f.write(str(processed_data))
