3bcca25c5fabeae9ea64071b7d17f8dbb4693fb3
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 18:11:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ workbook = openpyxl.load_workbook('input_data.xlsx')
 # Select the first sheet
 sheet = workbook.active
 
-# Read the name and age values from the first row
-name = sheet.cell(row=1, column=1).value
-age = sheet.cell(row=1, column=2).value
+# Prompt the user to enter the name and age values
+name = input("Enter name: ")
+age = input("Enter age: ")
 
-# Print the values
-print(f"Name: {name}, Age: {age}")
+# Read the name and age values from the first row of the sheet
+expected_name = sheet.cell(row=1, column=1).value
+expected_age = sheet.cell(row=1, column=2).value
+
+# Check if the user's input matches the expected values
+if name == expected_name and age == expected_age:
+    print("Input is correct.")
+else:
+    print("Input is incorrect.")
