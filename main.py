3b5ccc4b4a714549483a8eb0ed041880b3923ea6
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 09:25:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ workbook = openpyxl.load_workbook('input_data.xlsx')
 # Select the first sheet
 sheet = workbook.active
 
-# Get the value from cell A1
-cell_value = sheet['A1'].value
+# Prompt the user to enter the input value
+input_value = input("Enter the input value: ")
 
 # Print the input value
-print(f"Input value: {cell_value}")
+print(f"Input value: {input_value}")
