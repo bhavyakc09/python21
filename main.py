bafc8de74e3d0655b9fc809e2ac5d2587091d4e5
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 10:28:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import sys
+import os
 import openpyxl
 
+# Get the current working directory
+cwd = os.getcwd()
+
+# Define the path to the input file relative to the current working directory
+input_file_path = os.path.join(cwd, 'input_data.xlsx')
+
 # Load the workbook
-workbook = openpyxl.load_workbook('input_data.xlsx')
+workbook = openpyxl.load_workbook(input_file_path)
 
 # Select the first sheet
 sheet = workbook.active
 
-# Read the cell reference from command line arguments
-if len(sys.argv) < 2:
-    print("Usage: python main.py [cell reference (e.g. A1)]")
-    exit()
-
-cell_reference = sys.argv[1]
+# Prompt the user to enter the cell reference
+cell_reference = input("Enter the cell reference (e.g. A1): ")
 
 # Read the value from the corresponding cell
 value = sheet[cell_reference].value
