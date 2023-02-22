64d3d9966ffa8b74bd53cc2eb74b522c109b5f75
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 09:43:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ workbook = openpyxl.load_workbook('input_data.xlsx')
 # Select the first sheet
 sheet = workbook.active
 
-# Prompt the user to enter the cell coordinates
-cell_coordinates = input("Enter the cell coordinates (e.g. A1): ")
+# Prompt the user to enter the cell reference
+cell_reference = input("Enter the cell reference (e.g. A1): ")
 
-# Read the value from the corresponding cell
-value = sheet[cell_coordinates].value
+# Get the value from the corresponding cell
+value = sheet[cell_reference].value
 
 # Print the input value
-print(f"{cell_coordinates}: {value}")
+print(f"Value at {cell_reference}: {value}")
