4a07d8d909e6e23bad911d454fe9002ecd1868f8
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-22 09:39:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import openpyxl
-import sys
 
 # Load the workbook
 workbook = openpyxl.load_workbook('input_data.xlsx')
@@ -7,12 +6,8 @@ workbook = openpyxl.load_workbook('input_data.xlsx')
 # Select the first sheet
 sheet = workbook.active
 
-# Read the input coordinates from the command line argument
-if len(sys.argv) < 2:
-    print("Usage: python main.py <cell_coordinates>")
-    exit(1)
-
-cell_coordinates = sys.argv[1]
+# Prompt the user to enter the cell coordinates
+cell_coordinates = input("Enter the cell coordinates (e.g. A1): ")
 
 # Read the value from the corresponding cell
 value = sheet[cell_coordinates].value
